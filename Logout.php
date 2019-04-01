5add37559d72a1049672bf4b1884489757bf9e31
Brahim kanouche <bkano020@uottawa.ca>
2019-04-01 18:09:30 -0400

    refactored log out and linked it back to log in

--- Logout.php
+++ Logout.php
@@ -1,23 +1,43 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>COJO OLYMPIC GAMES :: LOG OUT PAGE</title>
+    <?php include "inc/resources.php" ?>
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <h1>COJO OLYMPICS</h1>
+        </header>
+
+        <a href="member_login.php" class="LOG IN AGAIN "> LOG IN BACK ?</a>
+
+        <?php
 	
-	// Initialize the session
-	session_start();
+			// Initialize the session
+			session_start();
 
-	// Unset all of the session variables
-	$_SESSION = array();
+			// Unset all of the session variables
+			$_SESSION = array();
 
-	if($_SESSION['loggedin'])
-	{		 
-			// Destroy the session.
-			session_destroy();
-			 
-			// Redirect to login page
-			header("location: member_login.php");
-			exit;
-			echo "<p id='connectMsg' class='alert alert-success'>YOU ARE ALREADY LOGGED OUT, PLEASE ENTER YOUR USERNAME AND PASSWORD TO LOG IN AGAIN !</p>";
-	}
-	else 
-	{
-		echo "<p id='connectMsg' class='alert alert-success'> ====> YOU ARE ALREADY LOGGED OUT</p>";
-	}
-?>
+			if(isset ($_SESSION['loggedin']) && $_SESSION['loggedin'])
+			{		 
+					// Destroy the session.
+					session_destroy();
+					 
+					// Redirect to login page
+					header("location: member_login.php");
+					exit;
+					echo "<p id='connectMsg' class='alert alert-success'>YOU ARE ALREADY LOGGED OUT, PLEASE ENTER YOUR USERNAME AND PASSWORD TO LOG IN AGAIN !</p>";
+			}
+			else 
+			{
+				echo "<p id='connectMsg' class='alert alert-success'> =====> LOGGED OUT !</p>";
+			}
+		?>
+
+        
+    </div>
+</body>
+</html>
