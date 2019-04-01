b71861604f872c2932f947cf2a9061ff40c023ba
Brahim kanouche <bkano020@uottawa.ca>
2019-04-01 15:03:30 -0400

    Cleaned DB and modified insertion

--- sql/createData.sql
+++ sql/createData.sql
@@ -1,13 +1,15 @@
 SET search_path = cojoDatabase;
 
+---Officiel
+
 INSERT INTO Officiel VALUES (DEFAULT, 'Joseph','Morinho' , '7 Daly ave', '22 Laurier','Entraineur'); -- 1
 INSERT INTO Officiel VALUES (DEFAULT, 'Diego','Simeoni' , '3 summerset', '12 king Edward','Entraineur'); -- 2
+
+---Athlete
 
 INSERT INTO Athlete VALUES (DEFAULT, 'james','Rodriguez' , '11 blvd', '17 cumberland','5' , 'Canada',1); -- 1
 INSERT INTO Athlete VALUES (DEFAULT, 'Roger','Federer' , '3 private', '21 rockland','12' , 'Swiss',2); -- 2
 INSERT INTO Athlete VALUES (DEFAULT, 'Usain', 'Bolt', '7 henderson', '71 Blake', '21', 'Jamaica',1); -- 3
-
-
 
 ---Installation
 
@@ -16,24 +18,31 @@ INSERT INTO Installation VALUES (DEFAULT, 'MontPetit Field', '801 King Adward av
 INSERT INTO Installation VALUES (DEFAULT, 'Minto Field', '801 King Adward ave', 'For Tennis', 'Tainings & Tests', '20'); -- 3
 INSERT INTO Installation VALUES (DEFAULT, 'MontPetit Field', '801 King Adward ave', 'For FootBall', ' & Tests', '20'); -- 4
 
+---ServiceMedical
 
 INSERT INTO ServiceMedical VALUES(DEFAULT, 'Marie-Curie Clinic', 'General inquieres', '105 Marie-Curie ave', '555-555-4456');
 INSERT INTO ServiceMedical VALUES(DEFAULT, 'Marie', 'General inquieres', '105 Marie-Curie ave', '555-555-4456');
 INSERT INTO ServiceMedical VALUES(DEFAULT, '-Curie Clinic', 'General inquieres', '105 Marie-Curie ave', '555-555-4456');
 
------
+---Employee
+
 INSERT INTO Employee VALUES (DEFAULT, 'Villeneuve', 'Myriam', 'OTTAWA', 'MONTREAL', '1234567890');
 INSERT INTO Employee VALUES (DEFAULT, 'MonNom', 'MonPrenom', '1234 rue Université', '900-A Ladouceur', '8139240559');
 INSERT INTO Employee VALUES (DEFAULT, 'LeDoyen', 'Monsieur', '4434 Soleil', '578 Vanier', '9009009090');
 INSERT INTO Employee VALUES (DEFAULT, 'Nom1', 'Prenom1', 'AdresseP1', 'AdresseV1', '0000000000');
 
+---Epreuve
 
 INSERT INTO Epreuve VALUES (DEFAULT, '100 m', 'course', 1);
 INSERT INTO Epreuve VALUES (DEFAULT, 'papillon','nage',2);
 
+---Residence
+
 INSERT INTO Residence VALUES (DEFAULT, 'A', '100', 'Moisture Farm, Tatooine', '613-616-6666'); -- 1
 INSERT INTO Residence VALUES (DEFAULT, 'B', '80', 'Jedi Temple, Coruscant', '613-111-7777'); -- 2
 INSERT INTO Residence VALUES (DEFAULT, 'C', '80', 'Citadel, Naboo', '616-616-8888'); -- 3
+
+---ServiceTransport
 
 INSERT INTO ServiceTransport VALUES (DEFAULT, '3000-01-01 17:30', '3000-01-01 19:00', 'Residence - Monpetit', '15 min');
 INSERT INTO ServiceTransport VALUES (DEFAULT, '3000-01-01 19:00', '3000-01-01 21:30', 'Minto - Monpetit', '20 min');
--- sql/createSchema.sql
+++ sql/createSchema.sql
@@ -3,24 +3,6 @@ CREATE SCHEMA cojoDatabase;
 
 SET search_path = cojoDatabase;
 
-CREATE TABLE Secretary (
-    id SERIAL PRIMARY KEY,
-    firstName VARCHAR(255) NOT NULL,
-    lastName VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    tel VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE Doctor (
-    id SERIAL PRIMARY KEY,
-    firstName VARCHAR(255) NOT NULL,
-    lastName VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    tel VARCHAR(255) NOT NULL,
-    specialty VARCHAR(255),
-    secretary INTEGER REFERENCES Secretary ON DELETE SET NULL
-);
-
 CREATE TABLE Officiel(
     id SERIAL PRIMARY KEY,
     pprenom VARCHAR(255) NOT NULL,
@@ -41,55 +23,6 @@ CREATE TABLE Athlete(
     officiel INTEGER REFERENCES Officiel ON DELETE SET NULL
 );
 
-CREATE TABLE Drug (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    price NUMERIC(11, 2) NOT NULL,
-    substance VARCHAR(255) NOT NULL,
-    generic BOOLEAN NOT NULL
-);
-
-CREATE TABLE DrugScript (
-    id SERIAL PRIMARY KEY,
-    drug INTEGER REFERENCES Drug NOT NULL,
-    doctor INTEGER REFERENCES Doctor NOT NULL, -- replace with appointment
-    --patient INTEGER REFERENCES Patient NOT NULL,
-    date DATE NOT NULL,
-    validDays INTEGER
-);
-
-CREATE TABLE ProcScript (
-    id SERIAL PRIMARY KEY,
-    procName VARCHAR(255) NOT NULL,
-    doctor INTEGER REFERENCES Doctor NOT NULL,
-    --patient INTEGER REFERENCES Patient NOT NULL,
-    date DATE NOT NULL
-);
-
-CREATE TABLE DrugConflict (
-    id SERIAL PRIMARY KEY,
-    substance1 VARCHAR(255) NOT NULL,
-    substance2 VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE Pathology (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE DrugPathologyConflict (
-    id SERIAL PRIMARY KEY,
-    substance VARCHAR(255) NOT NULL,
-    pathology INTEGER REFERENCES Pathology NOT NULL
-);
-
-CREATE TABLE PatientPathology (
-    id SERIAL PRIMARY KEY,
-    --patient INTEGER REFERENCES Patient NOT NULL,
-    pathology INTEGER REFERENCES Pathology NOT NULL
-);
-
----------- Nos Tables 
 CREATE TABLE Installation(
 	id SERIAL PRIMARY KEY,
 	iNom CHAR(30) NOT NULL,
