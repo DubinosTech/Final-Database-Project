3722c7c2c927e3a872d0c4377f168a6e690bdb5e
Emmanuel Asinyo <34837163+emmanueldubien@users.noreply.github.com>
2019-04-01 18:08:26 -0400

    Changed Otions background

--- options.php
+++ options.php
@@ -5,12 +5,18 @@
 <head>
     <meta charset="UTF-8">
     <title>COJO OLYMPIC GAMES PROJECT - CSI2532 :: Options</title>
+    <style>
+        body {
+            background-image: url(" r/olympic3.jpg");
+        } </style>
     <?php include "inc/resources.php" ?>
 </head>
 <body>
     <div class="wrapper">
         <header>
             <h1>COJO OLYMPIC GAMES PROJECT - CSI2532</h1>
+            <h2>                                    DATABASE MANAGEMENT COJO OLYMPIC GAMES PROJECT - CSI2532             </h2>
+            
             <?php breadcrumb("Options") ?>
         </header>
 
